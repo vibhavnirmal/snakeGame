cd33fd261f8b44c18ed5af92bac0343714902351
Vibhav <vibhavknirmal@gmail.com>
2021-05-11 19:39:39 +0530

    basic setup

---
+++ snakeGame.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+// Main 4 functions every time creating a game:> setup draw input logic
+
+// global variables
+bool gameOver;
+const int width = 20;
+const int height = 20;
+
+int x, y, fruitX, fruitY, score;
+
+enum Direction
+{
+    STOP = 0,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
+Direction dir;
+
+void Setup()
+{
+    // Game Startup
+    gameOver = false;
+    dir = STOP;
+
+    x = width / 2;
+    y = height / 2;
+
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+
+    score = 0;
+}
+
+void Draw()
+{
+    system("cls");
+
+    for (int i = 0; i < width; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0)
+            {
+                cout << "#";
+            }
+            else if (j == width - 1)
+            {
+                cout << "#";
+            }
+            else if (i == y && j == x)
+            {
+                cout << "O";
+            }
+            else if (i == fruitY && j == fruitX)
+            {
+                cout << "f";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width; i++)
+        cout << "#";
+
+    cout << endl;
+}
+
+void Input()
+{
+}
+
+void Logic()
+{
+}
+
+int main()
+{
+    Setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+    }
+    return 0;
+}
